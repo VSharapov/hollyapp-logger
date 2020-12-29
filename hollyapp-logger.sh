ba652bf005fe467b5231ee7466e6a6d8265154c0
Vasiliy Sharapov <vasiliys@gmail.com>
2020-12-29 13:01:43 +0000

    Don't reset branch every time

--- hollyapp-logger.sh
+++ hollyapp-logger.sh
@@ -9,7 +9,7 @@ for listID in $(cat target.list | tr '\n' ' '); do
   # First let's make sure we're on the right branch
   while [[ "$(git rev-parse --abbrev-ref HEAD)" != "$listBranch" ]]; do
     #DEBUG# echo "The current branch is \"$(git rev-parse --abbrev-ref HEAD)\" and not \"${listBranch}\""
-    git checkout -B "${listBranch}"
+    git checkout -b "${listBranch}" || git checkout "${listBranch}"
     #DEBUG# echo "Tried to checkout \"${listBranch}\", now on \"$(git rev-parse --abbrev-ref HEAD)\""
     #DEBUG# echo "Enter to continue, ^C to quit"
     #DEBUG# read
